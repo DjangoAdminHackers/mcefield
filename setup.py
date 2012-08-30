d1dbcd880a2087209f40be928b52e0397101fe6d
andybak <andy@andybak.net>
2012-08-30 11:50:45 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='mcefield',
+      version='0.1',
+      description='MCEField',
+      author='Andy Baker',
+      author_email='andy@ixxy.co.uk',
+      packages=find_packages(),
+      include_package_data=True,
+)
