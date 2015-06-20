4c11832726ba941f00c15fdc6152faca3acb02b3
Andy Baker <andy@andybak.net>
2015-06-20 10:17:49 +0100

    cleanup and remove some functionality specific to ixxy cms and adminskin (will move that code into the respective repo)

--- mcefield/static/js/mce_global.js
+++ mcefield/static/js/mce_global.js
@@ -3,26 +3,12 @@ if(typeof($) === "undefined" && typeof(django.jQuery) != "undefined"){var $ = dj
 if (typeof(site_mce_config) == 'undefined'){
     var site_mce_config = {}
 }
-var extra_styles = site_mce_config.extra_styles || "Grey text=grey"; // TODO make configurable
-var extra_classes = site_mce_config.extra_classes || "class<grey"; // TODO make configurable
+var extra_styles = site_mce_config.extra_styles || "Regular=dummystyle"; // TODO make configurable
+var extra_classes = site_mce_config.extra_classes || "class<dummystyle"; // TODO make configurable
 var content_width = site_mce_config.content_width || 690; // TODO this should relate to site's content width to give accurate idea of line lengths
 var table_controls =  site_mce_config.table_controls || ", tablecontrols";
 var extra_plugins = site_mce_config.extra_plugins || ", table";
 var table_elements = site_mce_config.table_elements || ",table,tr,th,#td,thead,tbody";
-
-function fix_banner() { //TODO this is specific to my admin CSS which has a position:fixed header
-    // Bigger offset if two toolbars are visible
-    if ($('.mceToolbarRow1:visible').length) {
-        if ($('.mceToolbarRow2:visible').length) {
-            offset = '153px';
-        } else {
-            offset = '123px';
-        }
-    } else {
-        offset = '88px';
-    }
-    $('#content').css('margin-top', offset);
-}
 
 function cleanup_html(element_id, html, body) {
     html = html.replace(/<!--[\s\S]+?-->/gi,'');//remove Word comments like conditional comments etc
@@ -38,57 +24,32 @@ function cleanup_html(element_id, html, body) {
     return $('<div>').append(content.clone()).html();
 }
 
-function CustomFileBrowser(field_name, url, type, win) {
-
-    var cmsURL = "/admin/filebrowser/browse/?pop=2&dir=images";
-    cmsURL = cmsURL + "&type=" + type;
-
-    tinyMCE.activeEditor.windowManager.open({
-        file: cmsURL,
-        width: 820,  // Your dimensions may differ - toy around with them!
-        height: 500,
-        resizable: "yes",
-        scrollbars: "yes",
-        inline: "yes",  // This parameter only has an effect if you use the inlinepopups plugin!
-        close_previous: "no"
-    }, {
-        window: win,
-        input: field_name,
-        editor_id: tinyMCE.selectedInstance.editorId
-    });
-    return false;
-}
-
 tinyMCE_config = {
 	mode : "none",
 	theme : "advanced",
-    skin : "thebigreason",
-    //skin_variant : "clean",
+  skin : "thebigreason",
 	theme_advanced_resizing : true,
 	theme_advanced_resize_horizontal : false,
 	theme_advanced_path : false,
 	theme_advanced_statusbar_location : "bottom",
-	content_css : "/static/stylesheets/mcestyles.css?" + new Date().getTime(),
-    theme_advanced_styles : extra_styles,
-	theme_advanced_toolbar_location : "external",
-	theme_advanced_toolbar_align : "left",
+	content_css : "/static/stylesheets/mcestyles.css?" + new Date().getTime(), // TODO Fix quick and dirty cache-busting
+  theme_advanced_styles : extra_styles,
 	theme_advanced_buttons1 : "formatselect,styleselect,removeformat,|,bold,italic,|,bullist,numlist,blockquote,|,undo,redo,|,link,unlink,anchor,|,image,fileBrowser,|,pdw_toggle",
 	theme_advanced_buttons2 : "charmap,hr,|,search,replace,|,code,visualchars,|"+table_controls,
 	theme_advanced_buttons3 : "",
 	theme_advanced_blockformats : "p,h2,h3",
-    width : content_width+18,
+  width : content_width+18,
 	cleanup_on_startup : true,
 	convert_urls : false,
-    fix_list_elements : true,
-    fix_nesting : true,
-    fix_table_elements : true,
+  fix_list_elements : true,
+  fix_nesting : true,
+  fix_table_elements : true,
 	gecko_spellcheck : true,
-    use_native_selects: false,
+  use_native_selects: false,
 	external_image_list_url : "/admin/cms/imagelist.js",
 	external_link_list_url : "/admin/cms/linklist.js",
 	auto_cleanup_word : true,
-    //plugins : "inlinepopups, paste, searchreplace, advimagescale, visualchars, autoresize, pdw"+extra_plugins,
-    plugins : "inlinepopups, paste, searchreplace, advimagescale, visualchars, autoresize, noneditable, pdw"+extra_plugins,
+  plugins : "inlinepopups, paste, searchreplace, advimagescale, visualchars, autoresize, noneditable, pdw"+extra_plugins,
 	valid_elements : ("-h2/h1[___],-h3/h4/h5[___],p[___],ul[___],-li,-ol,blockquote,br,-em/i,-strong/b,-span[!___],-div[!___],a[!name|!href|title|target],hr,img[src|class<left?right?center?floatleft?floatright|alt|title|height|width]"+table_elements).replace(/___/g, extra_classes),
     paste_preprocess : function(pl, o) {
         o.content = o.content.replace(/<!(?:--[\s\S]*?--\s*)?>\s*/g,'');
@@ -98,66 +59,30 @@ tinyMCE_config = {
     pdw_toggle_on : 1,
     pdw_toggle_toolbars : "2",
     setup : function(ed) {
-        ed.addButton('showWhitespace', {
-            title : 'Show Whitespace',
-            image : '/static/images/admin/show_whitespace.gif', // TODO make a proper icon
-            onclick : function() {
-                if (!tinyMCE.activeEditor.show_paragraphs) {
-                    tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('p, h1, h2, h3, h4, ul, li, ol'), 'background-color', '#FFFFBB');
-                    tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('p, h1, h2, h3, h4, ul, ol'), 'margin-bottom', '5px');
-                    tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'background-color', '#AAF');
-                    tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'border', '1px solid blue');
-                    tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'margin', '2px');
-                    tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'padding', '2px');
-                    tinyMCE.activeEditor.show_paragraphs = true;
-                } else {
-                    tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('p, h1, h2, h3, h4, ul, li, ol'), 'background-color', 'transparent');
-                    tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('p, h1, h2, h3, h4, ul, ol'), 'margin-bottom', '0');
-                    tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'background-color', '#FFF');
-                    tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'border', 'none');
-                    tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'margin', '0');
-                    tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'padding', '0');
-                    tinyMCE.activeEditor.show_paragraphs = false;
-                }
-            }
-        });
-        ed.addButton('fileBrowser', {
-            title : 'Open Filebrowser',
-            image : '/static/js/tiny_mce/themes/advanced/img/iframe.gif',
-            onclick : function() {
-                //window.open('/admin/filebrowser/browse/?pop=ixxy', 'Filebrowser', 'height=500,width=980,resizable=yes,scrollbars=yes');
-                //window.open('/admin/filebrowser/browse/?pop=tinymce', 'Filebrowser', 'height=500,width=980,resizable=yes,scrollbars=yes');
-//                if (!$('a#fancylink').length) {
-//                    $('div:first').after('<a style="display: none" id="fancylink" class="iframe" href="/admin/filebrowser/browse/?pop=tinymce&dir=images"></a>');
-//                }
-//                $("a#fancylink")
-//                        .fancybox({
-//                            padding: 0,
-//                            width: 820,
-//                            height: 500,
-//                            titleShow: false
-//                        })
-//                        .trigger("click");
-                tinyMCE.activeEditor.windowManager.open({
-                    file: "/admin/filebrowser/browse/?pop=0",
-                    //andyb mce filebrowser integration: file: "/admin/filebrowser/browse/?pop=2&ixxy=1&dir=images",
-                    width: 820,
-                    height: 500,
-                    resizable: "yes",
-                    scrollbars: "yes",
-                    inline: "yes",
-                    close_previous: "no"
-                }, {});
-            }
-        });
-
-        ed.onActivate.add(function(ed) {
-            fix_banner();
-        });
-
-        ed.onClick.add(function(ed) {
-            fix_banner();
-        });
+        
+      ed.addButton('showWhitespace', {
+          title : 'Show Whitespace',
+          image : '/static/images/admin/show_whitespace.gif', // TODO make a proper icon
+          onclick : function() {
+              if (!tinyMCE.activeEditor.show_paragraphs) {
+                  tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('p, h1, h2, h3, h4, ul, li, ol'), 'background-color', '#FFFFBB');
+                  tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('p, h1, h2, h3, h4, ul, ol'), 'margin-bottom', '5px');
+                  tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'background-color', '#AAF');
+                  tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'border', '1px solid blue');
+                  tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'margin', '2px');
+                  tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'padding', '2px');
+                  tinyMCE.activeEditor.show_paragraphs = true;
+              } else {
+                  tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('p, h1, h2, h3, h4, ul, li, ol'), 'background-color', 'transparent');
+                  tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('p, h1, h2, h3, h4, ul, ol'), 'margin-bottom', '0');
+                  tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'background-color', '#FFF');
+                  tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'border', 'none');
+                  tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'margin', '0');
+                  tinyMCE.activeEditor.dom.setStyle(tinyMCE.activeEditor.dom.select('a'), 'padding', '0');
+                  tinyMCE.activeEditor.show_paragraphs = false;
+              }
+          }
+      });
 
         ed.onPreInit.add(function(ed){
             if(typeof(fontFaceCssTag) !== 'undefined') {
