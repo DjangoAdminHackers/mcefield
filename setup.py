e935cb6ee6d458d1da377f7ea23d0fce4ad40c7a
Andy Baker <andy@andybak.net>
2015-07-06 15:31:07 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name='mcefield',
-      version='0.1',
+      version='0.2',
       description='MCEField',
       author='Andy Baker',
       author_email='andy@ixxy.co.uk',
